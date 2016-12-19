db20aa9f7e440c58aad511c37b091344b6e4416e
Dennis Hamilton <orcmid@apache.org>
2016-12-19 09:42:01 -0800

    0.0.0 Initial setup with Orcmid's Preferences

---
+++ BullCowGame/BlueCowGame.cpp
@@ -0,0 +1,17 @@
+/* BlueCowGame.cpp 0.0.0              UTF-8                        2016-12-19*/
+/* ------1---------2---------3---------4---------5---------6---------7------ */
+
+
+
+
+
+
+/* 0.0.0 2016-12-19-09:16 Create Initial Setup for Orcmid's Favorite Style 
+         There is a Git repository at the Section02 project level.
+		 The initial templates is Visual C++ > General > Empty Project.
+		 BlueCowGame is the single startup project of Section02.
+		 Warning Level 4 (/W4) is set for the BlueCowGame C/C++ options
+		 Unicode Character Set is selected for the Project
+		 Editor settings are for soft tabs every 4 columns, and the horizontal
+         ruler in line 2 is manually honored in my layouts.
+         */
