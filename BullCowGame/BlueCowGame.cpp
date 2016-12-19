a5b31d165518e0b2a9e2da13250a347c9284a5ec
Dennis Hamilton <orcmid@apache.org>
2016-12-19 09:51:33 -0800

    0.0.1 Confirmation of GitHub setup and publishing
    The repo was created with VS 2015 Community.  I added it via GitHub for
    Windows and published it to my GitHub account.

--- BullCowGame/BlueCowGame.cpp
+++ BullCowGame/BlueCowGame.cpp
@@ -1,4 +1,4 @@
-/* BlueCowGame.cpp 0.0.0              UTF-8                        2016-12-19*/
+/* BlueCowGame.cpp 0.0.1              UTF-8                        2016-12-19*/
 /* ------1---------2---------3---------4---------5---------6---------7------ */
 
 
@@ -6,10 +6,15 @@
 
 
 
-/* 0.0.0 2016-12-19-09:16 Create Initial Setup for Orcmid's Favorite Style 
+/* 0.0.1 2016-12-19-09:50 Added Section02 Project to GitHub for Windows
+         The solution, with the default .gitignore, is published to my
+         GitHub Account.  Some touchups and review of what is kept in Git.
+         The Section02-BullCow-GDD.txt file is maintained at project level.
+   0.0.0 2016-12-19-09:16 Create Initial Setup for Orcmid's Favorite Style 
          There is a Git repository at the Section02 project level.
 		 The initial templates is Visual C++ > General > Empty Project.
 		 BlueCowGame is the single startup project of Section02.
+         BlueCowGame.cpp is used instead of main.cpp ... just because.
 		 Warning Level 4 (/W4) is set for the BlueCowGame C/C++ options
 		 Unicode Character Set is selected for the Project
 		 Editor settings are for soft tabs every 4 columns, and the horizontal
