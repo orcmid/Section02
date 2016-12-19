3d8cee030bb9c4ae3df0215a252b1e6913a095ab
Dennis Hamilton <orcmid@apache.org>
2016-12-19 10:17:50 -0800

    0.0.2 Complete skeleton for working through the further exercises
    Some workarounds needed because I started with a Visual C++ > General >
    Empty template.
    
    This is now aligned with the course Section02 Lecture 14.

--- BullCowGame/BlueCowGame.cpp
+++ BullCowGame/BlueCowGame.cpp
@@ -1,12 +1,32 @@
-/* BlueCowGame.cpp 0.0.1              UTF-8                        2016-12-19*/
+/* BlueCowGame.cpp 0.0.2              UTF-8                        2016-12-19*/
 /* ------1---------2---------3---------4---------5---------6---------7------ */
 
+#include <iostream>
+
+int main() 
+{   /* Skeleton to demonstrate successful build and operation */
+
+    std::cout << "Welcome to the Holiday Camp with Bulls and Cows\n"
+                 "and games to play.\n\n";
+
+    std::cout << "Press Enter to quit\n";
+    std::cin.ignore();
+
+    return 0;
+}
+
+/* TODO:
+     * Make a README.md for the GitHub folks
+     * Build the skeleton using explicit name qualification for now.
+     * Create a shortcut file in the project for launching the program
+       in a Windows command shell window that does not close automatically,
+       working around a problem when General > Empty Project is used.
+     /
 
 
-
-
-
-/* 0.0.1 2016-12-19-09:50 Added Section02 Project to GitHub for Windows
+/* 0.0.2 2016-12-19-10:14 Initial Build success.  Added hack to wait for
+         input so console shell does not exit automatically.
+   0.0.1 2016-12-19-09:50 Added Section02 Project to GitHub for Windows
          The solution, with the default .gitignore, is published to my
          GitHub Account.  Some touchups and review of what is kept in Git.
          The Section02-BullCow-GDD.txt file is maintained at project level.
@@ -20,3 +40,5 @@
 		 Editor settings are for soft tabs every 4 columns, and the horizontal
          ruler in line 2 is manually honored in my layouts.
          */
+
+/*                   ***** end of BullCowGame.cpp *****                      */
