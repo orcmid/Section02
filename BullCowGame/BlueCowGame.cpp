27e22117c70eb91be57e3bd7fad88f121a9fb48a
Dennis Hamilton <orcmid@apache.org>
2016-12-20 13:55:51 -0800

    0.0.4 Start separating out functions, with notes and deviations of mine on naming things.

--- BullCowGame/BlueCowGame.cpp
+++ BullCowGame/BlueCowGame.cpp
@@ -1,4 +1,4 @@
-/* BlueCowGame.cpp 0.0.2              UTF-8                        2016-12-19*/
+/* BlueCowGame.cpp 0.0.4              UTF-8                       2016-12-20 */
 /* ------1---------2---------3---------4---------5---------6---------7------ */
 
 #include <iostream>
@@ -8,22 +8,16 @@ using namespace std;
 
 const int WordSize = 5;
 
+
+
+void IntroduceGame();
+string GetValidGuess();
+
 int main() 
-{   
-    // INTRODUCE THE GAME
-    cout
-        << "Welcome to Bulls and Cows, a fun word game.\n"
-        << "Guess a secret word having " << WordSize << " letters,"
-        << " all different.\n";
+{
+    IntroduceGame();
 
-    // GET A GUESS FROM THE PLAYER
-    string Guess = "";
-    cout << "\n Your guess? ";
-    getline(cin, Guess);
-
-    // REPEAT THE GUESS BACK TO THEM
-    cout << "\nYou guessed: " << Guess << endl;
-
+    string Guess = GetValidGuess();
 
     // compensate for using different Project template for this code:
     cout << "\nPress Enter to quit\n";
@@ -32,15 +26,63 @@ int main()
     return 0;
 }
 
+/* INTRODUCE THE GAME */
+void IntroduceGame()
+{
+    cout
+        << "Welcome to Bulls and Cows, a fun word game.\n"
+        << "Guess a secret word having " << WordSize << " letters,"
+        << " all different.\n";
+    return;
+}
+
+// GET A PROPER GUESS FROM THE PLAYER
+   /* The guess must have the correct number of letters without
+      duplicates.  It might be checked against a word list, but not now. 
+      If the user chooses not to enter a guess, we return an empty string.
+      Another way would be to return a different string that is impossible
+      to have entered. 
+      */
+
+string GetValidGuess()
+{
+    string Guess = "";
+    cout << "\n Your guess? ";
+    getline(cin, Guess);
+        // verification and whether to quit or not needed here.
+    cout << "\nYou guessed: " << Guess << endl;
+
+        /* We don't need this, but it is not clear whether
+           the checking can be here or has to follow.  I would
+           like it all on one line somehow, so guesses and Bulls+Cows are
+           in an easy-to-use column.
+           This changes the abstraction and coupling between getting a guess
+           and reporting its strength.
+           */
+        /* TODO: Failing verification of the input should not lead to a
+                 return.  We don't want it to count as a try until a 
+                 good try is returned, so that will be handled in the
+                 bounded loop, once we know how to limit guesses based
+                 on word size. 
+                 */
+
+    return Guess;
+}
+
+
 /* TODO:
      * Make a README.md for the GitHub folks
-     * Build the skeleton using explicit name qualification for now.
      * Consider letting the student choose the maximum number of letters,
        based on what there is in a directory of word choices.
+     * NOT DOING NOW: A big hashtable dictionary which is randomly probed to
+       get qualifying words.  I bet I can use the hash algorithm from Adv10.
+       Then I need to get/make a dictionary of isograms.
      /
 
 
-/* 0.0.3 2016-12-19-21:42 Lesson 17 alignment.  Using different nomenclature
+/* 0.0.4 2016-12-20-13:53 Lesson 18 alignment, with many notes.  I am 
+         deviating, but not by much.
+   0.0.3 2016-12-19-21:42 Lesson 17 alignment.  Using different nomenclature
          and WordSize spelling.  Got getline working.
    0.0.2 2016-12-19-10:14 Initial Build success.  Added hack to wait for
          input so console shell does not exit automatically.
